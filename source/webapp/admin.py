5f8259892af4983dfe3dc753cfca6b4a9d432e93
Said <s.avkhadeev@gmail.com>
2021-09-16 17:43:26 +0600

    Added models

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from webapp.models import Issue, IssueType, IssueStatus
 
-# Register your models here.
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ["id", "summary", "time_created"]
+    list_filter = ["id"]
+    search_fields = ["summary"]
+    fields = ["summary", "description", "time_created"]
+    readonly_fields = ["time_created"]
+
+
+admin.site.register(Issue, IssueAdmin)
+admin.site.register(IssueType)
+admin.site.register(IssueStatus)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Issue(models.Model):
+    summary = models.CharField(max_length=150, verbose_name='Summary')
+    description = models.CharField(max_length=2000, verbose_name='Description')
+    status = models.ForeignKey('webapp.IssueStatus', on_delete=models.RESTRICT,
+                               verbose_name='Status', related_name='statuses')
+    type = models.ForeignKey('webapp.IssueType', on_delete=models.RESTRICT,
+                             verbose_name='Type', related_name='types')
+    time_created = models.DateTimeField(auto_now_add=True)
+    time_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.pk} - {self.summary}"
+
+
+class IssueType(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Type')
+
+
+class IssueStatus(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Status')
