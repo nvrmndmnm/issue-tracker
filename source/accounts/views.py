46444eacb8ea835e712047db69f94b5fe256d8b7
Said <s.avkhadeev@gmail.com>
2021-10-18 15:08:20 +0600

    HW-58: Добавил передачу параметра next страницами логина и регистрации друг другу

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -15,11 +15,12 @@ class RegisterView(CreateView):
     def form_valid(self, form):
         user = form.save()
         login(self.request, user)
-        return redirect('webapp:index')
+        return redirect(self.get_success_url())
 
     def get_success_url(self):
         next_url = self.request.GET.get('next')
         if not next_url:
+            print(next_url)
             next_url = self.request.POST.get('next')
         if not next_url:
             next_url = reverse('webapp:index')
