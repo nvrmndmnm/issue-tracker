f2d5432a4a0910a7c8e65cabcd88a22fff1f9b8e
Said <s.avkhadeev@gmail.com>
2021-10-25 09:56:43 +0600

    HW-60: Добавил страницу пользователя, ограничил права, добавил частичный шаблон для списка проектов

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import login
 from django.contrib.auth import get_user_model
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.views.generic import CreateView, DetailView, ListView
@@ -36,12 +36,13 @@ class UserListView(PermissionRequiredMixin, ListView):
     permission_required = 'auth.view_user'
 
 
-class UserDetailView(LoginRequiredMixin, DetailView):
+class UserDetailView(UserPassesTestMixin, DetailView):
     model = get_user_model()
     template_name = 'user_detail.html'
     context_object_name = 'user_obj'
     paginate_related_by = 5
     paginate_related_orphans = 0
+    permission_required = 'auth.view_user'
 
     def get_context_data(self, **kwargs):
         projects = self.object.users.order_by('-id')
@@ -52,3 +53,7 @@ class UserDetailView(LoginRequiredMixin, DetailView):
         kwargs['projects'] = projects
         kwargs['is_paginated'] = page.has_other_pages()
         return super().get_context_data(**kwargs)
+
+    def test_func(self):
+        return self.request.user == self.get_object() or\
+               self.request.user.has_perm(self.permission_required)
