81c8d4b8386a352f38bdac1e1072b97fd59105cf
Said <s.avkhadeev@gmail.com>
2021-10-28 15:53:07 +0600

    HW-61: Добавил страницу изменения профиля пользователя, переименовал и обновил частичный шаблон формы

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -1,5 +1,8 @@
 from django import forms
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+
+from accounts.models import Profile
 
 
 class RegisterForm(UserCreationForm):
@@ -14,3 +17,15 @@ class RegisterForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
         fields = ['username', 'password1', 'password2', 'first_name', 'last_name', 'email']
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ['first_name', 'last_name', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from accounts.views import RegisterView, UserListView, UserDetailView
+from accounts.views import RegisterView, UserListView, UserDetailView, UserUpdateView
 
 app_name = 'accounts'
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('create/', RegisterView.as_view(), name='create'),
-    path('<int:pk>/', UserDetailView.as_view(), name='profile')
+    path('<int:pk>/', UserDetailView.as_view(), name='profile'),
+    path('<int:pk>/update/', UserUpdateView.as_view(), name='update')
 ]
